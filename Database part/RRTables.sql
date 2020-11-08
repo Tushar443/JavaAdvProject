b23d4a60c43d1e7793b7ebdf6452c1135926c4ba
Tushar443 <moretushart33@gmail.com>
2020-11-08 07:33:38 +0530

    project exam

--- Database part/RRTables.sql
+++ Database part/RRTables.sql
@@ -5,7 +5,9 @@ drop table user;
 
 select * from user;
 select * from train;
-select * from class_type;
+select * from class_type where train_id=7;
+
+delete from class_type where price=0;
 
 delete from user where user_id=3;
 
@@ -13,11 +15,20 @@ desc train;
 desc user;
 desc class_type;
 
-insert into class_type (class_name,price,train_id) values("gen",150,1);
-insert into class_type (class_name,price,train_id) values("ac1",200,1);
-insert into class_type (class_name,price,train_id) values("ac2",250,1);
-insert into class_type (class_name,price,train_id) values("fc",350,1);
-insert into class_type (class_name,price,train_id) values("slp",400,1);
+insert into class_type (class_name,price,train_id) values("gen",150,5);
+insert into class_type (class_name,price,train_id) values("ac1",200,5);
+insert into class_type (class_name,price,train_id) values("ac2",250,5);
+insert into class_type (class_name,price,train_id) values("fc",350,5);
+insert into class_type (class_name,price,train_id) values("slp",400,5);
+
+insert into class_type (class_name,price,train_id) values("gen",150,6);
+insert into class_type (class_name,price,train_id) values("ac1",200,6);
+insert into class_type (class_name,price,train_id) values("ac2",250,6);
+insert into class_type (class_name,price,train_id) values("fc",350,6);
+insert into class_type (class_name,price,train_id) values("slp",400,6);
+
+
+
 
 create table train(
 	train_id int auto_increment not null primary key,
@@ -34,7 +45,7 @@ create table train(
 );
 
 
-
+ 
 create table user(
 	user_id int auto_increment not null primary key,
     user_name varchar(30),
@@ -47,3 +58,4 @@ create table user(
 
 
 
+update class_type set price=200 where class_name="gen" and train_id=1;
